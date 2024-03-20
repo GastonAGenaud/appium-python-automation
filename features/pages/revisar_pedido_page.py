f4709248b44be6746d187bba86a1d41a298b225b
Rodrigo <rockdrick999@gmail.com>
2024-03-20 18:09:45 -0300

    some changes

--- features/pages/revisar_pedido_page.py
+++ features/pages/revisar_pedido_page.py
@@ -2,4 +2,4 @@ from features.pages.base_page import Page
 from appium.webdriver.common.mobileby import MobileBy
 
 
-class RevisarPedidoPage(Page):
+# class RevisarPedidoPage(Page):
--- src/application.py
+++ src/application.py
@@ -1,10 +1,10 @@
-from features.pages.login_page import LoginPage
+# from features.pages.login_page import LoginPage
 from features.pages.main_page import MainPage
-from features.pages.inicio_sesion_page import InicioSesionPage
-from features.pages.modificar_recorrido_page import ModificarRecorridoPage
-from features.pages.rebajar_pedido_page import RebajarPedidoPage
-from features.pages.revisar_pedido_page import RevisarPedidoPage
-from features.pages.anular_pedido_page import AnularPedidoPage
+# from features.pages.inicio_sesion_page import InicioSesionPage
+# from features.pages.modificar_recorrido_page import ModificarRecorridoPage
+# from features.pages.rebajar_pedido_page import RebajarPedidoPage
+# from features.pages.revisar_pedido_page import RevisarPedidoPage
+# from features.pages.anular_pedido_page import AnularPedidoPage
 from features.pages.entregar_pedido_page import EntregarPedidoPage
 from features.pages.cuadrar_page import CuadrarPage
 from features.pages.empty_states_page import EmptyStatesPage
@@ -12,13 +12,13 @@ from features.pages.empty_states_page import EmptyStatesPage
 
 class Application:
     def __init__(self, driver):
-        self.login_page = LoginPage(driver)
+        # self.login_page = LoginPage(driver)
         self.main_page = MainPage(driver)
-        self.inicio_sesion_page = InicioSesionPage(driver)
-        self.modificar_recorrido_page = ModificarRecorridoPage(driver)
-        self.rebajar_pedido_page = RebajarPedidoPage(driver)
-        self.revisar_pedido_page = RevisarPedidoPage(driver)
-        self.anular_pedido_page = AnularPedidoPage(driver)
+        # self.inicio_sesion_page = InicioSesionPage(driver)
+        # self.modificar_recorrido_page = ModificarRecorridoPage(driver)
+        # self.rebajar_pedido_page = RebajarPedidoPage(driver)
+        # self.revisar_pedido_page = RevisarPedidoPage(driver)
+        # self.anular_pedido_page = AnularPedidoPage(driver)
         self.entregar_pedido_page = EntregarPedidoPage(driver)
         self.cuadrar_page = CuadrarPage(driver)
         self.empty_states_page = EmptyStatesPage(driver)
