1c554aef96746b9df0e3d3fd1c2bc8083082083f
Rodrigo <rockdrick999@gmail.com>
2024-07-31 12:04:10 -0300

    algunos cambios

--- features/steps/empty_states_steps.py
+++ features/steps/empty_states_steps.py
@@ -31,7 +31,7 @@ def valido_texto(context, texto):
         assert bool(context.app.cuadrar_page.valido_mensaje_entrega_impecable())
     elif texto == '¡Felicitaciones! Has entregado el pedido sin rebajas. Que siga la buena racha.':
         assert bool(context.app.cuadrar_page.validar_entrega_exitosa_texto())
-    elif texto == 'Hola conductor-01':
+    elif texto == 'Buenas tardes':
         assert bool(context.app.inicio_sesion_page.valid_mensaje_saludo())
     elif texto == 'Estas son tus vueltas disponibles':
         assert bool(context.app.inicio_sesion_page.valid_titulo_vueltas_disponible())
